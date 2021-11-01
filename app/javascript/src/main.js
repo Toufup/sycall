2acf36d859631c86c14311e523a0dc6d375af9a9
Toufup <lucrei123tu@gmail.com>
2021-11-01 15:35:00 +0900

    add: Vuetify を導入した

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,9 +1,15 @@
-import Vue from 'vue/dist/vue.esm'
+import Vue from 'vue'
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 import App from '../App.vue'
 Vue.config.productionTip = false;
 
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
+
 document.addEventListener('DOMContentLoaded', () => {
     new Vue({
+        vuetify,
         el: "#app",
         render: h => h(App),
     })
