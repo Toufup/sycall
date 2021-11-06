ec9a9188339c2293ee05a2b305c634e53c4ac09d
Toufup <lucrei123tu@gmail.com>
2021-11-06 13:34:42 +0900

    add: 検索結果を選択するときのチェックマークを追加した。
    理由：選択したことがわかるようにしたいため。
    
    備考：選択されていない状態だと丸が表示。

--- app/frontend/components/Search.vue
+++ app/frontend/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="subtitle">
-        <div id="search-bar">
+        <div>
             <h2 class="black--text">どのコールで練習しますか？</h2>
             <v-text-field placeholder="曲名、アーティスト名で検索する" 
                 clearable clear-icon="mdi-close-circle"
@@ -9,31 +9,40 @@
                 id="search-input" v-model="keyword"
             ></v-text-field>
         </div>
-        <!-- TODO E 結果を選択した時に、末尾にチェックマークを付ける -->
-        <!-- TODO B リストアイテムをクリック後、Valueを入力欄に反映させる -->
-        <!-- TODO D 結果リストをオーバーフロー形式にする、最大heightは3行 -->
-        <!-- TODO A 結果がない場合の表示 -->
-        <div id="result-list">
-            <transition enter-active-class="animate__animated animate__fadeIn"
-            leave-active-class="animate__animated animate__fadeOut">
-            <!-- TODO C 理想：結果リストを浮かせる -->
-            <!-- TODO C 出来なければ：結果リストの位置を入力欄にくっつけるように調整する。 -->
-                <v-list rounded v-show="hasResult" class="rounded-xl mt-n7 ">
-                    <v-list-item-group color="primary" v-model="selected">
-                        <v-list-item v-for="result in results" :key="result.id">
-                            <v-list-item-content>
-                                <v-list-item-title>{{result.title}}</v-list-item-title>
-                                <v-list-item-subtitle>
-                                    アーティスト：{{result.artist}}
-                                </v-list-item-subtitle>
-                                <v-list-item-subtitle>
-                                    作成者：{{result.creator}}
-                                </v-list-item-subtitle>
-                            </v-list-item-content>
-                        </v-list-item>
-                    </v-list-item-group>
-                </v-list>
-            </transition>
+        <div>
+            <v-list v-show="isValid && !hasResult" rounded max-height="200px" class="rounded-xl overflow-y-auto mt-n7 mb-7">
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="font-weight-bold">一致するコールは見つかりませんでした</v-list-item-title>
+                        <v-list-item-subtitle>
+                            <span class="primary--text font-weight-bold">「フィードバック・ご意見」</span>
+                            から開発者にリクエストしましょう！♪
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        <!-- TODO A 必ず選択するバリデーションをつける -->
+            <v-list v-show="hasResult" rounded max-height="280px" class="rounded-xl overflow-y-auto mt-n7 mb-7">
+                <v-list-item-group color="primary">
+                    <v-list-item 
+                        v-for="result in results" :key="result.id" 
+                        @click="selectResult(result)" v-slot="{ active }">
+                        <v-list-item-icon>
+                            <v-icon large v-show="!active">mdi-checkbox-blank-circle-outline</v-icon>
+                            <v-icon large v-show="active">mdi-checkbox-marked-circle</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-content>
+                            <v-list-item-title>{{result.title}}</v-list-item-title>
+                            <v-list-item-subtitle>
+                                アーティスト：{{result.artist}}
+                            </v-list-item-subtitle>
+                            <v-list-item-subtitle>
+                                コール作成者：{{result.creator}}
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
         </div>
     </div>
 </template>
@@ -43,8 +52,9 @@
         name: "Search",
         data() {
             return {
+                isValid: false,
                 hasResult: false,
-                selected: null,
+                selectedResult: {},
                 keyword: "",
                 results: [],
                 calls: [
@@ -58,20 +68,26 @@
         },
         watch: {
             keyword(value){
-                // 入力値があれば判別ロジックに入る
-                if (value && value !== this.selected ) {
+                if (value && value.trim()) {
+                    this.isValid = true;
                     this.results = this.calls.filter((c) => {
-                        // タイトルで検索するロジック
-                        // TODO AA アーティスト名で検索するロジックを考える。
-                        return c.title.toLowerCase().indexOf(value.toLowerCase()) !== -1
+                        return c.title.toLowerCase().indexOf(value.trim().toLowerCase()) !== -1 || 
+                                c.artist.toLowerCase().indexOf(value.trim().toLowerCase()) !== -1
                     });
-                    // 結果があれば「結果あり」のフラグを、なければ「結果なし」のフラグをつける。
                     this.results.length > 0 ? this.hasResult = true : this.hasResult = false
                 } else {
-                    // TODO A 入力値がなければ判別ロジックに入らず、入力欄が初期状態であるフラグを設定
-                    this.hasResult = false
+                    this.hasResult = false;
+                    this.isValid = false;
                 }
             },
+        },
+        methods: {
+            selectResult(result){
+                this.selectedResult = result;
+            }
+        },
+        updated() {
+            console.log("Updated");
         },
     }
 </script>
