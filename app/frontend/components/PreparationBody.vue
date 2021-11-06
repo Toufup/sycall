62c8b438039ad1ec6547798a316230e11b1ba7f1
Toufup <lucrei123tu@gmail.com>
2021-11-06 19:49:50 +0900

    add: 2種類のYouTubeリンクのバリデーションを追加した。
    ・未入力時
    ・フォーマットが正しくない時

--- app/frontend/components/PreparationBody.vue
+++ app/frontend/components/PreparationBody.vue
@@ -4,16 +4,25 @@
             <v-container px-16 py-8>
                 <div id="musicVideo">
                     <h2 class="black--text">どのミュージックビデオで練習しますか？</h2>
+                    <v-alert v-if="showNoUrlAlert" dense rounded color="primary" 
+                        icon="mdi-information-outline" class="mb-2">
+                        URLを入力してください。
+                    </v-alert>
+                    <v-alert v-if="showUrlFormatAlert" dense rounded color="primary" 
+                        icon="mdi-information-outline" class="mb-2">
+                        URLの形式が正しくありません。YouTubeの動画リンクを正しく入力してください。
+                    </v-alert>
                     <v-text-field placeholder="YouTube動画のリンクを貼り付ける" 
                         autofocus clearable clear-icon="mdi-close-circle"
                         background-color="white" color="primary" rounded outlined
-                        prepend-inner-icon="mdi-youtube" class="mt-2"
+                        prepend-inner-icon="mdi-youtube" class="mt-2" v-model.trim="videoUrl"
                     ></v-text-field>
                 </div>
                 <Search></Search>
                 <v-row id="start-button" justify="center" align="center">
                     <v-btn color="primary" class="my-4 black--text"
                         depressed x-large rounded width="170px"
+                        @click="start"
                     >START</v-btn>
                 </v-row>
             </v-container>
@@ -25,9 +34,38 @@
     import Search from './Search.vue'
     export default {
         name: "PreparationBody",
+        data() {
+            return {
+                videoUrl: "",
+                showNoUrlAlert: false,
+                showUrlFormatAlert: false,
+            }
+        },
+        computed: {
+            hasNoUrlError(){
+                return this.videoUrl ? false : true
+            },
+            hasUrlFormatError(){
+                const urlReg = /^(https\:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)+[\S]{11}$/
+                if (this.hasNoUrlError) {
+                    return false
+                } else {
+                    return urlReg.test(this.videoUrl) ? false : true
+                }
+            },
+        },
         components: {
             Search,
-        }
+        },
+        methods: {
+            start(){
+                // URL が入力されていないと、アラートを表示する
+                this.showNoUrlAlert = this.hasNoUrlError ? true : false;
+                // URL の形式が正しくないと、アラートを表示する
+                this.showUrlFormatAlert = this.hasUrlFormatError ? true : false;
+                // 次のページに遷移する（プログラム的vue-router）
+            }
+        },
     }
 </script>
 
