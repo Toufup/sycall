4f012e3c5846e9e3c393b85338438a6b4e51e74e
Toufup <lucrei123tu@gmail.com>
2021-11-10 21:47:20 +0900

    add: 分かりやすくするためにコールの部分の字幕に背景色を付けた。
    このロジックは、コールすべき部分を【】で囲むことを前提にしている。

--- app/frontend/components/PracticeBody.vue
+++ app/frontend/components/PracticeBody.vue
@@ -26,7 +26,20 @@
                 <v-sheet v-show="this.isVisible" color="white rounded-pill px-5 py-2" min-height="56px" id="lyrics-wrapper">
                     <h2 class="text-center black--text" 
                         v-for="line in lyricsLines" :key="line.time" v-show="showLyric(line)"
-                    >{{line.lyric}}
+                    >
+                        <!-- v-forで回ってきた1行の歌詞を、どのようなHTMLタグで作成するか条件分岐する -->
+                        <!-- しかしその前に、この1行の歌詞はそもままでは条件分岐できない（どの言葉がコールの部分か区別がないため） -->
+                        <!-- ゆえに一回splitedLyricメソッドで加工し、返ってきたもので条件分岐する -->
+                        <span v-for="(word, index) in splitedLyric(line.lyric)" :key="index">
+                            <!-- 歌詞がコール歌詞だと検出したら、特殊なクラスを付与する -->
+                            <span v-if="matchCallLyrics(word)"
+                                class="pink" :style="{backgroundColor:callBgc}"
+                            >
+                                {{word}}
+                            </span>
+                            <!-- そうでない歌詞はそのまま展示する -->
+                            <span v-else>{{word}}</span>
+                        </span>
                     </h2>
                 </v-sheet>
             </v-col>
@@ -324,6 +337,7 @@
                     modestbranding: 1,
                 },
                 currentTime: 0,
+                callBgc: "",
             }
         },
         computed: {
@@ -368,6 +382,48 @@
                 if (this.currentTime + offset >= line.time && this.currentTime + offset <= nextLine.time) {
                     return true
                 }
+            },
+            splitedLyric(lyric){
+                // コール箇所（中括弧）にマッチする
+                const callReg = /【.*?】/g;
+                // コール箇所があれば歌詞の分断処理に入る、そうでなければそのまま配列に入れてreturn
+                if (callReg.test(lyric)) {
+                    // コール箇所以外にマッチする
+                    const notCallReg = /[^【】]+(?=【[^【】]+】)|[^【】]+$/g
+                    
+                    // コール箇所の歌詞だけが入っている配列、
+                    const callLyrics = lyric.match(callReg)
+                    // そうでない歌詞だけが入っている配列
+                    const notCallLyrics = lyric.match(notCallReg)
+
+                    // 上記両方を入れる容器を用意する
+                    const indexLyrics = {}
+    
+                    // まずはコール歌詞を入れていく、キー名はコール歌詞が元の歌詞で初めて現れたインデックス
+                    for(let i=0;i<callLyrics.length;i++){
+                        const index = lyric.indexOf(callLyrics[i])
+                        indexLyrics[index] = callLyrics[i]
+                    }
+                    for(let b=0;b<notCallLyrics.length;b++){
+                        const index = lyric.indexOf(notCallLyrics[b])
+                        indexLyrics[index] = notCallLyrics[b]
+                    }
+    
+                    // 上記の処理で全ての歌詞を「コールの歌詞」「そうでない歌詞」に分割できた
+                    // しかしindexLyricsはオブジェクトで、次のステップでは配列が望ましいので、下記にように変換する
+                    const resultArray = Object.keys(indexLyrics).map(function(key){
+                        return indexLyrics[key]
+                    })
+    
+                    // 結果をreturnする
+                    return resultArray
+                } else {
+                    return [lyric]
+                }
+            },
+            matchCallLyrics(word){
+                const callReg = /【.*?】/g;
+                return callReg.test(word)
             }
         },
         
