e236d12270c3aa3486f7e5bf8834906de6a7388c
Toufup <lucrei123tu@gmail.com>
2021-11-19 18:30:00 +0900

    add: songsテーブルにbpmカラムを追加した。

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ApplicationRecord
     belongs_to :artist
 
     validates :title, presence: true
+    validates :bpm, numericality: {only_integer: true}, allow_blank: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_19_085456) do
+ActiveRecord::Schema.define(version: 2021_11_19_091448) do
 
   create_table "artists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_11_19_085456) do
     t.bigint "artist_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "bpm"
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
