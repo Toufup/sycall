166b22d298ffbce39b64efad6c92ea3dd1631eab
Toufup <lucrei123tu@gmail.com>
2021-11-19 22:14:21 +0900

    fix: dependentオプションを追加した

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-    has_many :songs
+    has_many :songs, dependent: :destroy
 
     validates :name, presence: true, uniqueness: true
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
     belongs_to :artist
-    has_many :videos
+    has_many :videos, dependent: :destroy
 
     validates :title, presence: true
     validates :bpm, numericality: {only_integer: true}, allow_blank: true
