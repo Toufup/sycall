e5264259145fa133c4d386bfe03ce644931b7cc8
Toufup <lucrei123tu@gmail.com>
2021-11-24 16:07:53 +0900

    add: 練習画面created時、サーバに歌詞をリクエストし、lyricsLinesに格納する

--- app/controllers/calls_controller.rb
+++ app/controllers/calls_controller.rb
@@ -1,6 +1,14 @@
 class CallsController < ApplicationController
     def search
-        @calls = LyricsVersion.search_calls_info(params[:keyword])
+        @calls = LyricsVersion.search_calls_info(call_params[:keyword])
     end
-    #TODO コールid → get_lyrics アクション → 歌詞を加工し、Jsonをクライアントに返す
+
+    def get_lyrics
+        @call_lyrics = LyricsVersion.find(call_params[:callId]).lyric.body
+    end
+    
+    private
+    def call_params
+        params.permit(:format, :keyword, :callId)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#index"
   get "/calls/search", to: "calls#search", defaults: { format: :json }
+  get "/calls/get_lyrics", to: "calls#get_lyrics", defaults: { format: :json }
 end
