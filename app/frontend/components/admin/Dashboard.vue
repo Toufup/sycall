717bd53399303d7eafec81414c1f7c30d2e9bc11
Toufup <lucrei123tu@gmail.com>
2021-11-29 11:05:27 +0900

    improve: v-tab-itemsをvue-router形式に各コンポにリンクするようにした。
    速度改善のため。

--- app/frontend/components/admin/Dashboard.vue
+++ app/frontend/components/admin/Dashboard.vue
@@ -1,19 +1,20 @@
 <template>
     <v-container fluid id="admin-body">
+        <v-alert dismissible type="success" color="maccha" dense class="mx-16 rounded-xl" :value="successAlert">
+            追加しました！
+        </v-alert>
         <v-sheet class="mainColor rounded-xl mx-16">
             <v-container px-16 py-8>
-                <v-tabs vertical color="maccha"
-                    v-model="currentTab" class="rounded-l-xl"
-                >
-                    <v-tab v-for="tab in tabs" :key="tab.name">
-                        <v-icon left>mdi-{{tab.icon}}</v-icon>{{tab.name}}
-                    </v-tab>
-                    <v-tabs-items v-model="currentTab" class="rounded-r-xl">
-                        <v-tab-item v-for="comp in components" :key="comp">
-                            <component :is="comp"></component>
-                        </v-tab-item>
-                    </v-tabs-items>
-                </v-tabs>
+                    <v-tabs vertical color="maccha"
+                        v-model="currentTab" class="rounded-l-xl"
+                    >
+                        <v-tab v-for="tab in tabs" :key="tab.name" :to="{name: tab.route}">
+                            <v-icon left>mdi-{{tab.icon}}</v-icon>{{tab.name}}
+                        </v-tab>
+                        <v-tabs-items v-model="currentTab" class="rounded-r-xl">
+                            <router-view></router-view>
+                        </v-tabs-items>
+                    </v-tabs>
             </v-container>
         </v-sheet>
     </v-container>
@@ -35,13 +36,18 @@
         data() {
             return {
                 tabs:[
-                    {icon: "account-music", name: "アーティスト"},
-                    {icon: "music-clef-treble", name: "曲"},
-                    {icon: "file-document", name: "バージョン"},
-                    {icon: "music-box", name: "歌詞"},
+                    {icon: "account-music", name: "アーティスト", route: "artists"},
+                    {icon: "music-clef-treble", name: "曲", route: "songs"},
+                    {icon: "file-document", name: "バージョン", route: "callLyricsVersions"},
+                    {icon: "music-box", name: "歌詞", route: "callLyrics"},
                 ],
                 currentTab: "Artists",
-                components: ["Artists", "Songs", "CallLyricsVersions", "CallLyrics"],
+                successAlert: false,
+            }
+        },
+        methods: {
+            showSuccess(){
+                this.successAlert = true
             }
         },
     }
--- app/frontend/router/index.js
+++ app/frontend/router/index.js
@@ -4,6 +4,10 @@ import TopPageBody from '../components/TopPageBody.vue'
 import PreparationBody from '../components/PreparationBody.vue'
 import PracticeBody from '../components/PracticeBody.vue'
 import Dashboard from '../components/admin/Dashboard.vue'
+import Artists from '../components/admin/Artists.vue'
+import Songs from '../components/admin/Songs.vue'
+import CallLyricsVersions from '../components/admin/CallLyricsVersions.vue'
+import CallLyrics from '../components/admin/CallLyrics.vue'
 
 export default new VueRouter({
     routes: [
@@ -21,7 +25,30 @@ export default new VueRouter({
         },
         {
             path: "/admin",
+            redirect: {name: "artists"},
             component: Dashboard,
+            children: [
+                {
+                    path: "artists",
+                    name: "artists",
+                    component: Artists,
+                },
+                {
+                    path: "songs",
+                    name: "songs",
+                    component: Songs,
+                },
+                {
+                    path: "call-lyrics-versions",
+                    name: "callLyricsVersions",
+                    component: CallLyricsVersions,
+                },
+                {
+                    path: "call-lyrics",
+                    name: "callLyrics",
+                    component: CallLyrics,
+                },
+            ]
         }
     ]
 })
