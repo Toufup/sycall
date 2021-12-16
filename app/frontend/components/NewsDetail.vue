942cbcac39aea1c8e62b492372044f37d3e56383
Toufup <lucrei123tu@gmail.com>
2021-12-16 14:01:49 +0900

    improve: お知らせ本文の体裁を整えた

--- app/frontend/components/NewsDetail.vue
+++ app/frontend/components/NewsDetail.vue
@@ -2,24 +2,31 @@
     <v-container fluid id="preparation-body">
         <v-card color="mainColor" class="rounded-xl mx-16">
             <v-container class="pa-8 black--text">
-                <v-row justify="space-between">
+                <v-row justify="center">
                     <v-col cols="auto">
                         <v-btn depressed fab color="white" @click="pageBack">
                             <v-icon x-large color="maccha">mdi-arrow-left</v-icon>
                         </v-btn>
                     </v-col>
-                    <v-col cols="auto">
+                    <v-col cols="10" class="text-center">
                         <h1>{{news.title}}</h1>
                         <p>{{news.createdAt}}</p>
                     </v-col>
+                    <v-col cols="auto">
+                        <v-btn depressed fab color="transparent">
+                            <v-icon x-large color="transparent"></v-icon>
+                        </v-btn>
+                    </v-col>
                 </v-row>
-                <v-col cols="12">
-                    <v-card outlined class="rounded-xl" color="white">
-                        <v-card-text class="black--text">
-                            <p>{{news.body}}</p>
-                        </v-card-text>
-                    </v-card>
-                </v-col>
+                <v-row>
+                    <v-col cols="12">
+                        <v-card outlined class="rounded-xl" color="white">
+                            <v-card-text class="black--text" id="news-body">
+                                <p>{{news.body}}</p>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
             </v-container>
         </v-card>
     </v-container>
@@ -52,5 +59,9 @@
 <style scoped>
     p{
         margin: 0;
+        font-size: 17px;
+    }
+    #news-body{
+        white-space: pre-line;
     }
 </style>
